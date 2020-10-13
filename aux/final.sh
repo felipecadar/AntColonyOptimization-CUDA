21526b1092c55e6e6c0782910dff148af23c0358
Felipe Cadar Chamone <cadar@dcc.ufmg.br>
2020-10-12 21:15:30 -0300

    add final exps

---
+++ aux/final.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for i in $(seq 1 30);
+do
+  ./ACO bases_grafos/entrada1.txt 200 300 0.3 1 3 entrada1-"$i"-final
+done
+
+for i in $(seq 1 30);
+do
+  ./ACO bases_grafos/entrada2.txt 200 300 0.3 1 3 entrada2-"$i"-final
+done
+
+for i in $(seq 1 30);
+do
+  ./ACO bases_grafos/entrada3.txt 200 300 0.3 1 3 entrada3-"$i"-final
+done
+
